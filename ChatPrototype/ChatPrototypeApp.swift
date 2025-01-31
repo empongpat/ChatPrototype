018bdc8b6da3edd767fe28e5d0e616da3701c6b6
Em Pongpat <empongpat@gmail.com>
2025-01-31 23:24:26 +0800

    feat: create Xcode Swift app project

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Pongpat Weesommai on 31/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
