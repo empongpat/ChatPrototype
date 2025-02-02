4a5cc7b154d730d6b335adc6d237c67908bdee0a
Em Pongpat <empongpat@gmail.com>
2025-02-02 23:20:20 +0800

    feat: adds HStack and stylings for chat bubbles

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -9,16 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        HStack(alignment: VerticalAlignment.center) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            Text("Chat App").foregroundColor(Color.blue)
+                .font(.system(size: 32, weight: .black, design: .rounded))
+        }.padding(48)
+        VStack(spacing: 24) {
             Text("Hello, world!")
+                .foregroundColor(.white)
+                .font(.system(size: 20, weight: .bold, design: .rounded))
                 .padding()
-                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8.0))
+                .background(Color.green, in: RoundedRectangle(cornerRadius: 16.0))
             Text("The World of AI.")
+                .foregroundColor(.white)
+                .font(.system(size: 20, weight: .bold, design: .rounded))
                 .padding()
-                .background(Color.blue, in: RoundedRectangle(cornerRadius: 8.0))
+                .background(Color.blue, in: RoundedRectangle(cornerRadius: 16.0))
+            Text("I am scared boss.")
+                .foregroundColor(.white)
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+                .padding()
+                .background(Color.green, in: RoundedRectangle(cornerRadius: 16.0))
+            Text("Just chill and chat guys!")
+                .foregroundColor(.white)
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+                .padding()
+                .background(Color.blue, in: RoundedRectangle(cornerRadius: 16.0))
         }
         .padding()
     }
